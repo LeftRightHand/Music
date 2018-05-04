21f003c2d19ae80538a17faaeb6f630ea3c9cf35
DESKTOP-2T5T6AG\SSS <963292639@qq.com>
2018-05-04 20:23:31 +0800

    no message

--- src/components/listview/listview.vue
+++ src/components/listview/listview.vue
@@ -11,6 +11,13 @@
           </ul>
         </li>
       </ul>
+      <div class="list-shortcut">
+        <ul>
+          <li v-for="(item, index) in shortcutList" :data-index="index" class="item">
+            {{item}}
+          </li>
+        </ul>
+      </div>
     </scroll>
 </template>
 
@@ -28,7 +35,11 @@
         }
       },
       computed:{
-
+        shortcutList() {
+          return this.data.map((group)=>{
+            return group.title.substr(0,1)
+          })
+        }
       }
     }
 </script>
@@ -60,5 +71,20 @@
           border-radius 50%
         .name
           margin-left 20px
-
+    .list-shortcut
+      position absolute
+      z-index 30
+      right 0
+      top 50%
+      transform translateY(-50%)
+      width:20px
+      padding 2px 0
+      text-align center
+      background $color-background-d
+      font-family Helvetica
+      .item
+        padding 3px
+        line-height 1
+        color $color-text-l
+        font-size $font-size-small
 </style>
